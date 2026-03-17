0eb64978c0b109a5964f510e2c3309387a8f8b89
Mateusz <mateusz.rokitowski2004@gmail.com>
2026-03-17 03:25:42 +0100

    Task entity created

---
+++ src/main/java/pl/mati/taskintelligenceapi/TaskIntelligenceApiApplication.java
@@ -0,0 +1,13 @@
+package pl.mati.taskintelligenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskIntelligenceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskIntelligenceApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/mati/taskintelligenceapi/entity/Task.java
@@ -0,0 +1,33 @@
+package pl.mati.taskintelligenceapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    private String description;
+
+    private Boolean completed;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
